3013cad924de2cbe7ae42b0379327ebf20bfb71e
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-04-03 19:46:27 +0300

    work with redux

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,17 @@
 import { Button } from "components/Button";
+import { useSelector } from "react-redux";
+import { getStatusFilter } from "redux/selectors";
+import { statusFilters } from "redux/constants";
 import css from "./StatusFilter.module.css";
 
 export const StatusFilter= () =>{
+  const filter=useSelector(getStatusFilter)
+  // console.log(filter);
   return (
     <div className ={css.container}>
-<Button>All</Button>
-<Button>Active</Button>
-<Button>Completed</Button>
+<Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   )
 }
