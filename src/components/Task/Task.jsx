6cb0e69da2969f53c9a61d328d487791e99e8340
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-04-04 16:47:11 +0300

    add delete action

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,7 +1,11 @@
 import{MdClose} from "react-icons/md";
+import { useDispatch } from "react-redux";
+import { deleteTask } from "redux/actions";
 import css from "./Task.module.css";
 
 export const Task =({task}) => {
+  const dispatch = useDispatch();
+  const handleDelete = () =>dispatch(deleteTask(task.id))
   return(
     <div className={css.container}>
     <input
@@ -10,7 +14,7 @@ export const Task =({task}) => {
     checked={task.completed}
     />
     <p className={css.text}>{task.text}</p>
-    <button className={css.btn}>
+    <button className={css.btn} onClick={handleDelete}>
      <MdClose size={24} />
     </button>
     </div>
